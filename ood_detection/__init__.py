c8e99a85fd420cc7e6abd34d5cbef0011702877e
Louis Owen <louis.owen@yellow.ai>
2023-05-16 03:08:21 +0700

    Add mc_dropout

--- ood_detection/__init__.py
+++ ood_detection/__init__.py
@@ -1,3 +1,6 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
 from ood_detection.detector.trust_scores import TrustScores
 from ood_detection.detector.entropy import Entropy
 from ood_detection.detector.local_outlier_factor import LOF
@@ -6,5 +9,6 @@ from ood_detection.detector.msp import MSP
 from ood_detection.detector.doc import DOC
 from ood_detection.detector.adb import ADB
 from ood_detection.detector.knn import KNN
+from ood_detection.detector.mc_dropout import MCDropout
 
 from ood_detection.dataloaders.base import DataLoader
