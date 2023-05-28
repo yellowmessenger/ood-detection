48705fb38615ec794efd9ac7f5a46211cbc80c38
Louis Owen <louis.owen@yellow.ai>
2023-05-28 12:04:00 +0700

    optimize benchmark script

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -16,27 +16,30 @@ do
         done
     for feature_extractor in 'use' 'mpnet' 'bert'
     do
-        for use_best_ckpt in False True
+        for use_best_ckpt in True False
         do
             for is_ood_label_in_train in True False
             do
                 for detector in 'TrustScores' 'Entropy' 'LOF' 'BinaryMSP' 'MSP' 'DOC' 'ADB' 'KNN' 'BiEncoderCosine' 'BiEncoderLOF' 'BiEncoderMaha' 'BiEncoderEntropy' 'BiEncoderPCAEntropy' 'BiEncoderPCACosine' 'BiEncoderPCAEuclidean'
                 do
-                    output_dir='./benchmarking_results'
-                    echo 'Dataset:' $dataset
-                    echo 'Feature Extractor:' $feature_extractor
-                    echo 'Detector:' $detector
-                    echo 'Use Best Checkpoint Model:' $use_best_ckpt
-                    echo 'Add OOD in training data:' $is_ood_label_in_train 
-                    echo 'Output Dir:' $output_dir
-                    python benchmark.py \
-                            --output_dir $output_dir \
-                            --dataset $dataset \
-                            --detector $detector \
-                            --feature_extractor $feature_extractor \
-                            --use_best_ckpt $use_best_ckpt \
-                            --is_ood_label_in_train $is_ood_label_in_train \
-                            --ood_label 'oos'
+                    if [[ ($use_best_ckpt == True && ($detector == 'BinaryMSP' || $detector == 'DOC' || $detector == 'Entropy' || $detector == 'LOF' || $detector == 'MSP' || $detector == 'TrustScores')) || ($use_best_ckpt == False) ]]
+                    then
+                        output_dir='./benchmarking_results'
+                        echo 'Dataset:' $dataset
+                        echo 'Feature Extractor:' $feature_extractor
+                        echo 'Detector:' $detector
+                        echo 'Use Best Checkpoint Model:' $use_best_ckpt
+                        echo 'Add OOD in training data:' $is_ood_label_in_train 
+                        echo 'Output Dir:' $output_dir
+                        python benchmark.py \
+                                --output_dir $output_dir \
+                                --dataset $dataset \
+                                --detector $detector \
+                                --feature_extractor $feature_extractor \
+                                --use_best_ckpt $use_best_ckpt \
+                                --is_ood_label_in_train $is_ood_label_in_train \
+                                --ood_label 'oos'
+                    fi
                 done
             done
         done
